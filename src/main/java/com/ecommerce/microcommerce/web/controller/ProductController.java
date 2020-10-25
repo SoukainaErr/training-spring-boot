c3f070b8e8c6f95f50a4225231567950ab44076c
MouhadEss <mouhadessabbane@gmail.com>
2020-10-25 15:27:41 +0100

    partie 3 + swagger

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -2,12 +2,15 @@ package com.ecommerce.microcommerce.web.controller;
 
 import com.ecommerce.microcommerce.dao.ProductDao;
 import com.ecommerce.microcommerce.model.Product;
+import com.ecommerce.microcommerce.web.exceptions.ProduitGratuitException;
 import com.ecommerce.microcommerce.web.exceptions.ProduitIntrouvableException;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJacksonValue;
@@ -18,12 +21,10 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 
-@Api( description="API pour es opérations CRUD sur les produits.")
+@Api(value = "MicroCommerce", description="API pour des opérations CRUD sur les produits.")
 
 @RestController
 public class ProductController {
@@ -33,7 +34,7 @@ public class ProductController {
 
 
     //Récupérer la liste des produits
-
+    @ApiOperation(value = "get list of product", response = Iterable.class, tags = "listeProduits")
     @RequestMapping(value = "/Produits", method = RequestMethod.GET)
 
     public MappingJacksonValue listeProduits() {
@@ -53,32 +54,35 @@ public class ProductController {
 
 
     //Récupérer un produit par son Id
-    @ApiOperation(value = "Récupère un produit grâce à son ID à condition que celui-ci soit en stock!")
+    @ApiOperation(value = "Récupère un produit grâce à son ID à condition que celui-ci soit en stock!", tags = "afficherUnProduit")
     @GetMapping(value = "/Produits/{id}")
-
-    public Product afficherUnProduit(@PathVariable int id) {
+    public MappingJacksonValue afficherUnProduit(@PathVariable int id) {
 
         Product produit = productDao.findById(id);
 
         if(produit==null) throw new ProduitIntrouvableException("Le produit avec l'id " + id + " est INTROUVABLE. Écran Bleu si je pouvais.");
 
-        return produit;
+        MappingJacksonValue produitsFiltres = new MappingJacksonValue(produit);
+        FilterProvider listDeNosFiltres = new SimpleFilterProvider().addFilter("monFiltreDynamique", SimpleBeanPropertyFilter.serializeAllExcept("prixAchat"));
+        produitsFiltres.setFilters(listDeNosFiltres);
+
+        return produitsFiltres;
     }
 
 
 
 
     //ajouter un produit
+    @ApiOperation(value = "Create a product", tags = "ajouterProduit")
     @PostMapping(value = "/Produits")
-
     public ResponseEntity<Void> ajouterProduit(@Valid @RequestBody Product product) {
 
         Product productAdded =  productDao.save(product);
 
         if (productAdded == null )
             return ResponseEntity.noContent().build();
-        else if (productAdded.getPrix()==0)
-            return ResponseEntity.noContent().build();
+        else if(product.getPrix()==0)
+            throw new ProduitGratuitException("L'ajout d'un produit avec un prix de 0 est IMPOSSIBLE.");
 
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
@@ -88,13 +92,13 @@ public class ProductController {
 
         return ResponseEntity.created(location).build();
     }
-
+    @ApiOperation(value = "delete a product", tags = "supprimerProduit")
     @DeleteMapping (value = "/Produits/{id}")
     public void supprimerProduit(@PathVariable int id) {
 
         productDao.delete(id);
     }
-
+    @ApiOperation(value = "update a product", tags = "updateProduit")
     @PutMapping (value = "/Produits")
     public void updateProduit(@RequestBody Product product) {
 
@@ -103,6 +107,7 @@ public class ProductController {
 
 
     //Pour les tests
+    @ApiOperation(value = "test a function by trying to find a product by price", response = Iterable.class, tags = "testeDeRequetes")
     @GetMapping(value = "test/produits/{prix}")
     public List<Product>  testeDeRequetes(@PathVariable int prix) {
 
@@ -110,6 +115,7 @@ public class ProductController {
     }
 
     //calcule de la marge de chaque produit
+    @ApiOperation(value = "calculer la marge", tags = "calculerMargeProduit")
     @GetMapping(value = "/AdminProduits")
     public List<String> calculerMargeProduit(){
         List<Product> ps = productDao.findAll();
